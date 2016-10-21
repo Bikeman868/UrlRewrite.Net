5bede6bd39605e6f4cfec4b9655bbeb2753fe2a3
Martin Halliday <martin.bikeman@gmail.com>
2016-10-21 13:59:23 -0700

    Released to NuGet as version 1.2.3

--- Solution/TestSite/App_Code/Global.cs
+++ Solution/TestSite/App_Code/Global.cs
@@ -29,7 +29,7 @@ namespace TestSite.App_Code
 
             UrlRewrite.RewriteModule.Initialize(
                 null, 
-                url => true /*rawUrlsToTrace.Contains(url.ToLower())*/,
+                url => rawUrlsToTrace.Contains(url.ToLower()),
                 url => rewrittenUrlsToTrace.Contains(url.ToLower()));
         }
     }
--- Solution/UrlRewrite-Net40/Properties/AssemblyInfo.cs
+++ Solution/UrlRewrite-Net40/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.2.2")]
-[assembly: AssemblyFileVersion("1.2.2")]
+[assembly: AssemblyVersion("1.2.3")]
+[assembly: AssemblyFileVersion("1.2.3")]
