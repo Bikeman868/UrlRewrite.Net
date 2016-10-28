599276c00fcecfc5cc58815adb79dde0756ea396
Martin Halliday <martin.bikeman@gmail.com>
2016-10-28 13:46:33 -0700

    Released to NuGet as version 1.2.4

--- Solution/UrlRewrite-Net40/Properties/AssemblyInfo.cs
+++ Solution/UrlRewrite-Net40/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyVersion("1.2.4")]
+[assembly: AssemblyFileVersion("1.2.4")]
